8a1d45ffea822d1ca02cfa46a1fc0143691592fa
Vipelar <vipelar@vipelar.com>
2024-09-06 10:27:57 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
