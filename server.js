1ec3945d97d86f4853e32f3e1b40822731e2932c
Vipelar <vipelar@vipelar.com>
2024-09-11 03:19:58 +0000

    ライブラリの変更

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`)
 });
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/pages/**/*.{html, js}', './src/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#d396ed',
+      },
+    },
+  },
+  plugins: [],
+};
