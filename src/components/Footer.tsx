6a24638cae557240a7997dd9ca33a2b557ff86e2
suica-crux <autumn@vipelar.com>
2025-06-13 03:08:48 +0000

    リンクを修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,15 +7,9 @@ import ExternalLink from './ExternalLink';
 export default function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-600 mt-16 py-8 flex flex-col items-center">
-      <Text size="xl">Links</Text>
+      {/* <Text size="xl">Links</Text>
       <div className="flex space-x-6">
-        <ExternalLink
-          href="https://instagram.com/doshisha_chigaku"
-          customClass="hover:text-blue-500"
-        >
-          Instagram
-        </ExternalLink>
-      </div>
+      </div> */}
       <div className="text-center mt-4 text-sm">
         <Text colour="gray">
           &copy; 2025 Vipelar. All Rights Reserved.
