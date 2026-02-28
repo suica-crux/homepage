409eb5587f5b29925f7bfe989e6a1121d9547f4f
suica-crux <autumn@vipelar.com>
2026-02-28 18:54:26 +0900

    Next.jsからAstroへ移行

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,15 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-      'next.config.ts',
-    ],
+    ignores: ['node_modules/**', 'dist/**', '.astro/**'],
   },
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: { 'astro/no-set-html-directive': 'error' },
+  },
 ];
-
-export default eslintConfig;
